e7f8da7b54afa3cc97df9eb8e3f598f7b27b8680
mpasindu273 <mpasindu273@gmail.com>
2022-01-19 09:58:45 +0530

    added cesar cipher Signed-off-by: mpasindu273 <mpasindu273@gmail.com>

---
+++ testconnect/testconnect/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BookBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LibraryDataSet = New testconnect.libraryDataSet()
+        Me.ABindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BookTableAdapter = New testconnect.libraryDataSetTableAdapters.bookTableAdapter()
+        CType(Me.BookBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LibraryDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ABindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(100, 108)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.BookBindingSource, "Title", True))
+        Me.TextBox1.Location = New System.Drawing.Point(89, 62)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'BookBindingSource
+        '
+        Me.BookBindingSource.DataMember = "book"
+        Me.BookBindingSource.DataSource = Me.LibraryDataSet
+        '
+        'LibraryDataSet
+        '
+        Me.LibraryDataSet.DataSetName = "libraryDataSet"
+        Me.LibraryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ABindingSource
+        '
+        Me.ABindingSource.DataMember = "a"
+        '
+        'BookTableAdapter
+        '
+        Me.BookTableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(277, 328)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.BookBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LibraryDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ABindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ABindingSource As System.Windows.Forms.BindingSource
+
+    Friend WithEvents LibraryDataSet As testconnect.libraryDataSet
+    Friend WithEvents BookBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents BookTableAdapter As testconnect.libraryDataSetTableAdapters.bookTableAdapter
+
+End Class
---
+++ testconnect/testconnect/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim constr As String = "Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\user1\Desktop\testconnect\testconnect\Database1.mdf;Integrated Security=True"
+    Dim da As New SqlDataAdapter
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LibraryDataSet.book' table. You can move, or remove it, as needed.
+        Me.BookTableAdapter.Fill(Me.LibraryDataSet.book)
+        'TODO: This line of code loads data into the 'Database1DataSet.a' table. You can move, or remove it, as needed.
+
+        'TODO: This line of code loads data into the 'AddressBookDataSet.tblContacts' table. You can move, or remove it, as needed.
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        BookBindingSource.MovePrevious()
+        If BookBindingSource.IsBindingSuspended Then
+            MsgBox("No more records")
+        End If
+    End Sub
+End Class
